d683ac5fedf03c040fa1204bcaf110e9fc18accd
Yonghyun.Lee <yonghyun.lee@coway.com>
2023-02-02 14:29:17 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+  import Router from 'svelte-spa-router'
+  import TouchContainer from './lib/TouchContainer.svelte';
+</script>
+
+<Router routes={{
+}} />
+
+<div class="image-container">
+  <TouchContainer />
+</div>
+
+<style>
+  .image-container {
+    background-color: beige;
+    display: flex;
+    flex: 1;
+  }
+</style>
---
+++ src/lib/TouchContainer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Button } from "flowbite-svelte";
+
+  let id = 0;
+  $: fields = []
+  function handleClick (event: MouseEvent) {
+    fields = fields.concat({id: ++id, x: event.clientX, y: event.clientY});
+  }
+</script>
+
+<div class="touchable-field" on:mousedown={handleClick}></div>
+{#each fields as field (field.id) }
+  <Button style={`position: absolute; left: ${field.x}px; top: ${field.y}px;`}>{field.x}</Button>
+{/each}
+
+<style>
+  .touchable-field {
+    flex: 1;
+    position: relative;
+  }
+</style>
